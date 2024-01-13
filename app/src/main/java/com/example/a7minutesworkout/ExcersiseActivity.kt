17463e281693893185c3cd9bdfa58a3698433102
Anshu Singh <anshusingh7738@gmail.com>
2024-01-13 22:04:30 +0530

    working on adding exercise list with image

--- app/src/main/java/com/example/a7minutesworkout/ExcersiseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExcersiseActivity.kt
@@ -3,6 +3,7 @@ package com.example.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExcersiseBinding
 import java.util.Objects
@@ -13,7 +14,15 @@ class ExcersiseActivity : AppCompatActivity() {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+
     private var inputTime = 10
+    private var exerciseInputTime = 30
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+
+    private var exerciseList : ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +34,41 @@ class ExcersiseActivity : AppCompatActivity() {
         if(supportActionBar != null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-
+        exerciseList = Constants.defaultExerciseList()
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        setUpResetView(10000)
+        setUpResetView()
     }
 
-    private fun setUpResetView(timer: Long){
+    private fun setUpResetView(){
+        binding?.flProgressBar?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "GET READY FOR NEXT EXERCISE"
 
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
 
-        setRestProgress(timer)
+        setRestProgress(REST_TIMER)
     }
+
+    private fun setUpExerciseView(){
+
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.tvTitle?.text = "JUMPING JACK"
+
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgress(EXERCISE_TIMER)
+    }
+
 
     private fun setRestProgress(timer: Long){
         binding?.progressBar?.progress = restProgress
@@ -61,10 +88,37 @@ class ExcersiseActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
 
-                binding?.tvTitle?.text = "Exercise 1"
-                inputTime = 30
-                binding?.progressBar?.max = 30
-                setUpResetView(30000)
+                // Call the exercise again and again
+                currentExercisePosition++
+                exerciseProgress = 0
+                setUpExerciseView()
+            }
+
+        }.start()
+
+    }
+
+    private fun setExerciseProgress(timer: Long){
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        restTimer = object : CountDownTimer(timer, 1000) {
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = exerciseInputTime - exerciseProgress
+                binding?.tvTimerExercise?.text = (exerciseInputTime - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+
+                Toast.makeText(
+                    this@ExcersiseActivity,
+                    "30 Seconds are over, lets go to the rest view.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                //Take Rest for 10 second
+                setUpResetView()
+
             }
 
         }.start()
@@ -77,6 +131,11 @@ class ExcersiseActivity : AppCompatActivity() {
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
+        }
+
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
 
         binding = null
---
+++ app/src/main/java/com/example/a7minutesworkout/UitlsConstant.kt
@@ -0,0 +1,4 @@
+package com.example.a7minutesworkout
+
+const val  REST_TIMER: Long = 10000
+const val EXERCISE_TIMER : Long= 30000
