c1d9afbe7919b2495f51d0964b62953deb687be3
Anshu Singh <anshusingh7738@gmail.com>
2024-01-14 23:14:48 +0530

    added 12 exercise list and showing exercise after 10 sec rest for 30 sec exercise each.

--- app/src/main/java/com/example/a7minutesworkout/ExcersiseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExcersiseActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import com.example.a7minutesworkout.databinding.ActivityExcersiseBinding
 import java.util.Objects
 
@@ -35,36 +37,72 @@ class ExcersiseActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         exerciseList = Constants.defaultExerciseList()
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                showConfirmationDialog(
+                    this@ExcersiseActivity,
+                    R.string.are_you_sure_you_want_to_cancel,
+                    R.drawable.ic_cancel_order
+                ) {
+                    finish()
+
+                }
+            }
+        })
+
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            onBackPressed()
+            showConfirmationDialog(
+                this@ExcersiseActivity,
+                R.string.are_you_sure_you_want_to_cancel,
+                R.drawable.ic_cancel_order
+            ) {
+                finish()
+
+            }
         }
+
 
         setUpResetView()
     }
 
     private fun setUpResetView(){
-        binding?.flProgressBar?.visibility = View.VISIBLE
+
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.tvTitle?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseLabel?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseValue?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
-        binding?.tvTitle?.text = "GET READY FOR NEXT EXERCISE"
+        binding?.tvExerciseName?.visibility = View.INVISIBLE
+        binding?.exerciseImg?.visibility = View.GONE
 
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
+        binding?.tvUpComingExerciseValue?.text = exerciseList!![currentExercisePosition+1].getName()
+
 
         setRestProgress(REST_TIMER)
     }
 
     private fun setUpExerciseView(){
 
-        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvUpComingExerciseLabel?.visibility = View.INVISIBLE
+        binding?.tvUpComingExerciseValue?.visibility = View.INVISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
-        binding?.tvTitle?.text = "JUMPING JACK"
+        binding?.tvExerciseName?.visibility = View.VISIBLE
+        binding?.exerciseImg?.visibility = View.VISIBLE
 
         if(exerciseTimer != null){
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        binding?.exerciseImg?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
 
         setExerciseProgress(EXERCISE_TIMER)
     }
@@ -81,12 +119,6 @@ class ExcersiseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-
-                Toast.makeText(
-                    this@ExcersiseActivity,
-                    "Here now we will start the exercise.",
-                    Toast.LENGTH_SHORT
-                ).show()
 
                 // Call the exercise again and again
                 currentExercisePosition++
@@ -110,14 +142,19 @@ class ExcersiseActivity : AppCompatActivity() {
 
             override fun onFinish() {
 
-                Toast.makeText(
-                    this@ExcersiseActivity,
-                    "30 Seconds are over, lets go to the rest view.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                // TODO(Step 10 - Updating the view after completing the 30 seconds exercise.)
+                // START
+                if (currentExercisePosition < exerciseList?.size!! - 1) {
+                    setUpResetView()
+                } else {
 
-                //Take Rest for 10 second
-                setUpResetView()
+                    Toast.makeText(
+                        this@ExcersiseActivity,
+                        "Congratulations! You have completed the 7 minutes workout.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                // END
 
             }
 
--- app/src/main/java/com/example/a7minutesworkout/UitlsConstant.kt
+++ app/src/main/java/com/example/a7minutesworkout/UitlsConstant.kt
@@ -1,4 +1,38 @@
 package com.example.a7minutesworkout
 
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.example.a7minutesworkout.databinding.DialogMessageConfirmationBinding
+
 const val  REST_TIMER: Long = 10000
 const val EXERCISE_TIMER : Long= 30000
+
+var currentGlobalDialog: AlertDialog? = null
+/**
+ * message in resource
+ */
+fun showConfirmationDialog(cxt: Context?, message: Int, icon: Int, function: () -> Unit) {
+
+    cxt?.let { context ->
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setCancelable(false)
+        val binding: DialogMessageConfirmationBinding =
+            DialogMessageConfirmationBinding.inflate(LayoutInflater.from(context))
+        builder.setView(binding.root)
+        val dialog = builder.create()
+        currentGlobalDialog = dialog
+        dialog.window?.setBackgroundDrawableResource(R.drawable.round_corner_dialog)
+        binding.ivIcon.setImageDrawable(ContextCompat.getDrawable(context, icon))
+        binding.tvMessage.text = context.getString(message)
+        binding.buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        binding.buttonOk.setOnClickListener {
+            dialog.dismiss()
+            function()
+        }
+        dialog.show()
+    }
+}
